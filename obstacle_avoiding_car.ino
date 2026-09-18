e19a9af4dfcac20f6911cdee37bcb381e34c73ec
Rudra Patel <rudrapatel240607@gmail.com>
2026-09-18 21:23:12 +0530

    Enhance obstacle avoidance and motor control
    Refactored obstacle avoidance logic and added motor control functions.

--- obstacle_avoiding_car.ino
+++ obstacle_avoiding_car.ino
@@ -1,16 +1,37 @@
 //Declaration of variables related to Ultrasonic Sensor
-int trig_p = 7;
-int echo_p = 8;
-long duration = 0;
-int distance = 0;
+int trig_p = 7;//trigger pin
+int echo_p = 8;//echo pin
+long duration = 0;//time taken to recieve signal by ultrasonic sensor
+int distance = 0;//distance measured from ultrasonic sensor
+int least_distance = 12;//arbitrary 12 cm least distance parameter
+int scan_time = 300;//safe time for which ultrasonic sensor can obtain reading
 float sound_speed = 0.0343;//in cm / micro sec
 
 //declaration of variables related to servo motor
 #include <Servo.h>
 Servo myservo;  // create Servo object to control a servo
-int pos = 90;// variable to store the servo position
+int pos_left = 0;// variable to store the left servo position
+int pos_center = 90;// variable to store the center servo position
+int pos_right = 180;// variable to store the right servo position
 
-void distance_calculation()
+//declaring motor pins
+
+//left motor pins
+int ENA =5;
+int IN1 = 2;
+int IN2 = 4;
+
+//right motor pins
+int ENB = 6;
+int IN3 = 12;
+int IN4 = 13;
+
+//analog values for speed
+int forward_speed = 85;
+int turn_speed = 120;//higher than forward as turning requires more torque
+
+
+int distance_calculation()
 {
   digitalWrite(trig_p, LOW);
   delayMicroseconds(2);
@@ -21,93 +42,159 @@ void distance_calculation()
 
   duration = pulseIn(echo_p, HIGH);//inbuilt function that measures time taken to recieve sound
 
-  distance = (sound_speed * duration)/2;//formula to calculate lenght based on speed and time
+  distance = (sound_speed * duration)/2;//formula to calculate distance based on speed and time
 
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
   
-  delay(500);//delay between serial monitor readings
+  return distance;
+}
+
+void move_forward()
+{
+  //both motor move in same forward direction
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, forward_speed); 
+  analogWrite(ENB, forward_speed);
+}
+
+void turn_left()
+{
+  //to turn left right wheel moves forward and left wheel moves backwards
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, turn_speed); 
+  analogWrite(ENB, turn_speed);
+  delay(500);
+}
+
+void turn_right()
+{
+  //to turn right left wheel moves forward and right wheel moves backwards
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, turn_speed); 
+  analogWrite(ENB, turn_speed);
+  delay(500);
+}
+
+void stop_car()
+{
+  //no current goes to motor hence car stops
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0); 
+  analogWrite(ENB, 0);
+  delay(1000);
 }
 
 void initial_best_distance() // to measure best initial distance from start time
 {
-  int pos_left = 0;     //reversed servo mounting: 0 is left
-  int pos_center = 90;  //90 is straight
-  int pos_right = 180;  //180 is right
-
   int left_initial_dist = 0;
   int center_initial_dist = 0;
   int right_initial_dist = 0;
 
   //scan Left
   myservo.write(pos_left);
-  delay(300);
-  distance_calculation();
-  left_initial_dist = distance;
+  delay(scan_time);
+  left_initial_dist = distance_calculation();
 
   //scan center
   myservo.write(pos_center);
-  delay(300);
-  distance_calculation();
-  center_initial_dist = distance;
+  delay(scan_time);
+  center_initial_dist = distance_calculation();
 
   //scan right
   myservo.write(pos_right);
-  delay(300);
-  distance_calculation();
-  right_initial_dist = distance;
+  delay(scan_time);
+  right_initial_dist = distance_calculation();
 
-  //reset head to center
-  myservo.write(pos_center);
-
-  //to find best initial angle
-  int best_angle = pos_center; //default to straight 
+  myservo.write(pos_center);//reset to center
 
   if (left_initial_dist > center_initial_dist && left_initial_dist > right_initial_dist) 
   {
-    best_angle = pos_left;   // 0 degrees
+    turn_left();//car turns left
+    myservo.write(pos_center);
+    move_forward();//car moves forward at servo being at center position
   } 
   else if (right_initial_dist > center_initial_dist && right_initial_dist > left_initial_dist) 
   {
-    best_angle = pos_right;  // 180 degrees
+    turn_right();//car turns right
+    myservo.write(pos_center);
+    move_forward();//car moves forward at servo being at center position
   }
-
 }
+
 void servo_control()
 {
-  delay(500);
-  myservo.write(pos);
-  delay(200);
-  if(distance < 12)//arbitrary 12 cm least distance parameter
-  {
-    pos = 0;//to scan left
-    myservo.write(pos);
-    distance_calculation();
-    delay(200);
-    if(distance < 12)
-    {
-      pos = 180;//to scan right
-      myservo.write(pos);
-      distance_calculation();
-    }
+  int left_dist = 0;
+  int center_dist = 0;
+  int right_dist = 0;
 
+  myservo.write(pos_center);
+  center_dist = distance_calculation();
+
+  if (center_dist <= least_distance)
+  {
+    stop_car();//stopping to get stable reading
+
+    //scan left
+    myservo.write(pos_left);
+    delay(scan_time);
+    left_dist = distance_calculation();
+
+    //scan right
+    myservo.write(pos_right);
+    delay(scan_time);
+    right_dist = distance_calculation();
+
+    myservo.write(pos_center);
+    delay(scan_time);
+
+    if(left_dist > right_dist)
+    {
+      turn_left();
+      move_forward();
+    }
+    else
+    {
+      turn_right();
+      move_forward();
+    }
   }
   else
   {
-    pos = 90;
-    myservo.write(pos);
+    move_forward();
   }
 }
+
 void setup() {
-  Serial.begin(9600);
   pinMode(trig_p, OUTPUT);//trig pin sends out ultrasonic waves
   pinMode(echo_p, INPUT);//echo pin recieves ultrasonic waves
+
+  // all motor pins are output including pwm control
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
   myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+  delay(500);//gives servo time to settle at center position
   initial_best_distance();
 }
 
 void loop() {
-  distance_calculation();
   servo_control();
 }
